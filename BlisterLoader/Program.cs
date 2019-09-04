ca76afa0d3713c9fa27eda93c5b55f7c1bb3353d
Jack Baron <jackmbaron@gmail.com>
2019-09-04 03:47:48 +0100

    BlisterLoader converts legacy playlists

--- BlisterLoader/Program.cs
+++ BlisterLoader/Program.cs
@@ -1,8 +1,7 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using Blister;
+using Blister.Conversion;
 
 namespace BlisterLoader
 {
@@ -10,6 +9,40 @@ namespace BlisterLoader
     {
         static void Main(string[] args)
         {
+            if (args.Length == 0)
+            {
+                ExitMessage("Please drag a .bplist file onto this .exe", 1);
+            }
+
+            string path = args[0];
+            bool fileExists = File.Exists(path);
+            if (!fileExists)
+            {
+                ExitMessage("The specified file does not exist!", 1);
+            }
+
+            string text = File.ReadAllText(path);
+            var legacy = PlaylistConverter.LoadLegacyPlaylist(text);
+            var playlist = PlaylistConverter.ConvertLegacyPlaylist(legacy);
+
+            string newPath = path.Replace(".bplist", ".blist");
+            using (FileStream fs = File.Open(newPath, FileMode.OpenOrCreate))
+            {
+                using (MemoryStream ms = PlaylistLib.SaveStream(playlist))
+                {
+                    ms.CopyTo(fs);
+                    fs.Flush();
+                }
+            }
+        }
+
+        static void ExitMessage(string message, int exitCode)
+        {
+            Console.WriteLine(message);
+            Console.WriteLine("Press [ENTER] to exit...");
+            Console.ReadKey();
+
+            Environment.Exit(exitCode);
         }
     }
 }
