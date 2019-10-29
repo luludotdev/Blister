60f5279d092875b324cc9a3c87b15cae354bbbf7
Jack Baron <jackmbaron@gmail.com>
2019-10-29 04:36:30 +0000

    use primitive types

--- Blister/Types/Beatmap.cs
+++ Blister/Types/Beatmap.cs
@@ -12,7 +12,7 @@ namespace Blister.Types
         /// Beatmap type
         /// </summary>
         [JsonProperty("type")]
-        public string Type { get; set; }
+        public BeatmapType Type { get; set; }
 
         /// <summary>
         /// Date this entry was added to the playlist
@@ -24,13 +24,13 @@ namespace Blister.Types
         /// BeatSaver Key
         /// </summary>
         [JsonProperty("key", NullValueHandling = NullValueHandling.Ignore)]
-        public string Key { get; set; }
+        public int? Key { get; set; }
 
         /// <summary>
         /// Beatmap sha1sum
         /// </summary>
         [JsonProperty("hash", NullValueHandling = NullValueHandling.Ignore)]
-        public string Hash { get; set; }
+        public byte[] Hash { get; set; }
 
         /// <summary>
         /// Beatmap zip as bytes
